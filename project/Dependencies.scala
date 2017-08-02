766a3b56c42bdfdafc821f08f8d4c883023b2e05
Nicolò Bidotti <nicolo.bidotti@agilelab.it>
2017-08-02 11:02:26 +0200

    Work on dependencies
    - upgrade akka
    - remove unncessary dependencies from core module
    - sort by name

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,10 @@
 import sbt._
 
+// TODO finish cleanup
+// TODO finish sorting
+// TODO extract versions
 /*
- * Dependencies definitions.
+ * Dependencies definitions. Keep in alphabetical order.
  *
  * See project/Versions.scala for the versions definitions.
  */
@@ -45,153 +48,125 @@ object Dependencies {
 	// ===================================================================================================================
 	// Compile dependencies
 	// ===================================================================================================================
-	val playcore = "com.typesafe.play" %% "play" % Versions.play
-	val playws = "com.typesafe.play" %% "play-ws" % Versions.play
-	val playcache = "com.typesafe.play" %% "play-cache" % Versions.play
-	val playjson = "com.typesafe.play" %% "play-json" % Versions.play
-	val playserver = "com.typesafe.play" %% "play-netty-server" % Versions.play
-	val akkaStream = "com.typesafe.akka" %% "akka-stream-experimental" % Versions.akkaStreams
-	val akkaHttpCore = "com.typesafe.akka" %% "akka-http-core-experimental" % Versions.akkaStreams
 	val akkaActor = "com.typesafe.akka" %% "akka-actor" % Versions.akka
+	val akkaCamel = "com.typesafe.akka" %% "akka-camel" % Versions.akka
 	val akkaCluster = "com.typesafe.akka" %% "akka-cluster" % Versions.akka
 	val akkaContrib = "com.typesafe.akka" %% "akka-contrib" % Versions.akka
+	val akkaHttpCore = "com.typesafe.akka" %% "akka-http-core" % Versions.akkaHttp
 	val akkaRemote = "com.typesafe.akka" %% "akka-remote" % Versions.akka
 	val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % Versions.akka
-	val akkaCamel = "com.typesafe.akka" %% "akka-camel" % Versions.akka
+	val akkaStream = "com.typesafe.akka" %% "akka-stream" % Versions.akka
+	val apacheCommonsLang3 = "org.apache.commons" % "commons-lang3" % Versions.apacheCommonsLang3Version // remove?
+	val asynchttpclient = "com.ning" % "async-http-client" % "1.9.39" // remove?
 	val avro = "org.apache.avro" % "avro" % Versions.avro
-	
+	val camelElastic = "org.apache.camel" % "camel-elasticsearch" % Versions.camelElasticSearch
+	val camelKafka = "org.apache.camel" % "camel-kafka" % Versions.camelKafka
+	val camelQuartz2 = "org.apache.camel" % "camel-quartz2" % Versions.camelQuartz2
+	val camelWebsocket = "org.apache.camel" % "camel-websocket" % Versions.camelWebsocket
 	val elasticSearch = "org.elasticsearch" % "elasticsearch" % Versions.elasticSearch
 	val elasticSearchSpark = "org.elasticsearch" %% "elasticsearch-spark" % Versions.elasticSearchSpark
-	val jodaTime = "joda-time" % "joda-time" % Versions.jodaTime % "compile;runtime"
-	// ApacheV2
-	val jodaConvert = "org.joda" % "joda-convert" % Versions.jodaConvert % "compile;runtime"
-	// ApacheV2
+	val hbaseClient = "org.apache.hbase" % "hbase-client" % Versions.hbase
+	val hbaseCommon = "org.apache.hbase" % "hbase-common" % Versions.hbase
+	val hbaseServer = "org.apache.hbase" % "hbase-server" % Versions.hbase
+	val hbaseSpark = "org.apache.hbase" % "hbase-spark" % Versions.hbase
+	val httpmime = "org.apache.httpcomponents" % "httpmime" % "4.3.1" // TODO remove?
+	val jodaConvert = "org.joda" % "joda-convert" % Versions.jodaConvert
+	val jodaTime = "joda-time" % "joda-time" % Versions.jodaTime
 	val json4sCore = "org.json4s" %% "json4s-core" % Versions.json4s
-	// ApacheV2
 	val json4sJackson = "org.json4s" %% "json4s-jackson" % Versions.json4s
-	// ApacheV2
 	val json4sNative = "org.json4s" %% "json4s-native" % Versions.json4s
-	// ApacheV2
 	val kafka = "org.apache.kafka" %% "kafka" % Versions.kafka kafkaExclusions
-	// ApacheV2
-	val kafkaClients =
-		"org.apache.kafka" % "kafka-clients" % Versions.kafka kafkaExclusions
-	// ApacheV2
-	val kafkaStreaming =
-		"org.apache.spark" %% "spark-streaming-kafka" % Versions.spark sparkExclusions
-	// ApacheV2
-	//val logback = "ch.qos.logback" % "logback-classic" % Logback
-	//val logbackCore = "ch.qos.logback" % "logback-core" % Logback
-	val reactive_mongo = "org.reactivemongo" %% "reactivemongo" % Versions.reactiveMongo
-	// MIT
+	val kafkaClients = "org.apache.kafka" % "kafka-clients" % Versions.kafka kafkaExclusions
+	val kafkaStreaming = "org.apache.spark" %% "spark-streaming-kafka" % Versions.spark sparkExclusions
+	val log4jApi = "org.apache.logging.log4j" % "log4j-api" % "2.8.2" // TODO remove
+	val log4jCore = "org.apache.logging.log4j" % "log4j-core" % "2.8.2" // TODO remove
+	val log4jSl4jImpl = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.8.2" // TODO remove
+	val metrics = "com.yammer.metrics" % "metrics-core" % "2.2.0" // TODO upgrade?
+	val playcore = "com.typesafe.play" %% "play" % Versions.play // TODO remove
+	val playws = "com.typesafe.play" %% "play-ws" % Versions.play // TODO remove
+	val playcache = "com.typesafe.play" %% "play-cache" % Versions.play // TODO remove
+	val playjson = "com.typesafe.play" %% "play-json" % Versions.play // TODO remove
+	val playserver = "com.typesafe.play" %% "play-netty-server" % Versions.play // TODO remove
+	val reactiveMongo = "org.reactivemongo" %% "reactivemongo" % Versions.reactiveMongo
+	val scalaj = "org.scalaj" %% "scalaj-http" % "1.1.4" // TODO remove?
+	val scaldi = "org.scaldi" %% "scaldi-akka" % "0.3.3" // TODO remove?
 	val slf4jApi = "org.slf4j" % "slf4j-api" % Versions.slf4j
-	// MIT
-	val sparkML = "org.apache.spark" %% "spark-mllib" % Versions.spark sparkExclusions
-	// ApacheV2
-	val sparkSQL = "org.apache.spark" %% "spark-sql" % Versions.spark sparkExclusions
-	// ApacheV2
-	val sparkYarn = "org.apache.spark" %% "spark-yarn" % Versions.spark sparkExclusions
-	
-	// TODO verify license
 	val solr = "org.apache.solr" % "solr-solrj" % Versions.solr
-	// TODO misses support for scala 2.11
-	//val solrspark = "it.agilelab.bigdata.spark" % "spark-solr" % Versions.solrSpark sparkSolrExclusions
-	
-	val hbasespark = "org.apache.hbase" % "hbase-spark" % Versions.hbaseSpark
-	val hbasecommond = "org.apache.hbase" % "hbase-common" % Versions.hbaseSpark
-	val hbaseclient = "org.apache.hbase" % "hbase-client" % Versions.hbaseSpark
-	val hbaseserver = "org.apache.hbase" % "hbase-server" % Versions.hbaseSpark
-	
-	val asynchttpclient = "com.ning" % "async-http-client" % "1.9.39"
-	
-	val scaldi = "org.scaldi" %% "scaldi-akka" % "0.3.3"
-	val apacheCommonsLang3 = "org.apache.commons" % "commons-lang3" % Versions.apacheCommonsLang3Version
-	val camelKafka = "org.apache.camel" % "camel-kafka" % Versions.camelKafka
-	val camelWebsocket = "org.apache.camel" % "camel-websocket" % Versions.camelWebsocket
-	val camelElastic = "org.apache.camel" % "camel-elasticsearch" % Versions.camelElasticSearch
-	val camelQuartz2 = "org.apache.camel" % "camel-quartz2" % Versions.camelQuartz2
-	val zkclient = "com.101tec" % "zkclient" % "0.3"
-	
+	//val solrspark = "it.agilelab.bigdata.spark" % "spark-solr" % Versions.solrSpark sparkSolrExclusions TODO misses support for scala 2.11
+	val sparkCore = "org.apache.spark" %% "spark-core" % Versions.spark sparkExclusions
+	val sparkMLlib = "org.apache.spark" %% "spark-mllib" % Versions.spark sparkExclusions
+	val sparkSQL = "org.apache.spark" %% "spark-sql" % Versions.spark sparkExclusions
+	val sparkYarn = "org.apache.spark" %% "spark-yarn" % Versions.spark sparkExclusions
 	val typesafeConfig = "com.typesafe" % "config" % "1.3.0"
-	
-	val scalaj = "org.scalaj" %% "scalaj-http" % "1.1.4"
-	
-	val metrics = "com.yammer.metrics" % "metrics-core" % "2.2.0"
-	
-	val httpmime = "org.apache.httpcomponents" % "httpmime" % "4.3.1"
-	
-	val log4jApi = "org.apache.logging.log4j" % "log4j-api" % "2.8.2"
-	val log4jCore = "org.apache.logging.log4j" % "log4j-core" % "2.8.2"
-	val log4jSl4jImpl = "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.8.2"
-	
+	val zkclient = "com.101tec" % "zkclient" % "0.3"
+
 	// grouped dependencies, for convenience =============================================================================
-	val play = Seq(playcore, playws, playcache, playjson, playserver)
 	
-	val akka = Seq(akkaStream,
-	               akkaHttpCore,
-	               akkaActor,
-	               akkaCluster,
-	               akkaContrib,
-	               akkaRemote,
-	               akkaSlf4j,
-	               akkaCamel)
+	val akka = Seq(
+		akkaActor,
+		akkaCluster,
+		akkaContrib,
+		akkaRemote,
+		akkaSlf4j
+	)
 	
-	val spark = Seq(sparkSQL)
+	val spark = Seq(sparkCore, sparkMLlib, sparkSQL)
 	
 	val apachesolr = Seq(solr/*, solrspark*/)
 	
 	val json = Seq(json4sCore, json4sJackson, json4sNative)
 	
-	val logging = Seq(slf4jApi, log4jApi, log4jCore, log4jSl4jImpl)
+	val logging = Seq(slf4jApi)
 	
 	val time = Seq(jodaConvert, jodaTime)
 	
 	val elastic = Seq(elasticSearch)
 	
-	
 	// ===================================================================================================================
 	// Test dependencies
 	// ===================================================================================================================
-	val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % Versions.akka % "test" // ApacheV2
-	val akkaClusterTest = "com.typesafe.akka" %% "akka-multi-node-testkit" % Versions.akka % "test"
+	val akkaClusterTestKit = "com.typesafe.akka" %% "akka-multi-node-testkit" % Versions.akka % "test"
+	val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % Versions.akka % "test"
 	val scalatest = "org.scalatest" %% "scalatest" % Versions.scalaTest % "test"
 	
 	// grouped dependencies, for convenience =============================================================================
-	val test = Seq(akkaTestKit, akkaClusterTest, scalatest)
+	val test = Seq(akkaTestKit, akkaClusterTestKit, scalatest)
 	
 	// ===================================================================================================================
 	// Module dependencies
 	// ===================================================================================================================
-	val core = akka ++ logging ++ time ++ test ++ elastic ++ apachesolr ++
-		Seq(avro,
-		    reactive_mongo,
-		    kafka,
-		    playjson,
-		    playws,
-		    scaldi,
-		    apacheCommonsLang3,
-		    sparkYarn,
-			  sparkSQL,
-		    asynchttpclient,
-		    typesafeConfig)
+	val core = akka ++
+		logging ++
+		time ++
+		test ++
+		elastic ++ // TODO remove when switching to plugins
+		apachesolr ++ // TODO remove when switching to plugins
+		Seq(
+			avro,
+			kafka, // TODO remove when switching to plugins
+			playws,
+			reactiveMongo,
+			sparkSQL,
+			typesafeConfig
+		)
 	
-	val master = play ++ logging ++ time ++ json ++ elastic
+	/*
+	val master = logging ++ time ++ json ++ elastic
 	
-	val wasp_producers = akka ++ logging ++ test ++
-		Seq(playws)
+	val wasp_producers = akka ++ logging ++ test
 	
 	val wasp_consumers = json ++ test ++
 		Seq(kafka,
 		    kafkaStreaming,
-		    sparkML,
 		    spark,
 		    elasticSearchSpark,
 		    camelKafka,
 		    camelWebsocket,
 		    camelElastic,
 		    camelQuartz2,
-		    hbasespark,
-		    hbaseclient,
-		    hbaseserver,
-		    hbasecommond)
+		    hbaseSpark,
+		    hbaseClient,
+		    hbaseServer,
+		    hbaseCommon)
+	*/
 }
--- project/Versions.scala
+++ project/Versions.scala
@@ -1,13 +1,10 @@
 /*
- * Versions definitions.
+ * Versions definitions. Keep in alphabetical order.
  */
 object Versions {
-	val scala = "2.11.11"
-	val jdk = "1.7"
-	
-	val play = "2.4.8"
-	val akka = "2.3.14"
-	val akkaStreams = "0.11"
+	val akka = "2.4.19" // do not use akka 2.5+ until spark has removed their dependency on akka 2.3, otherwise master & consumer won't be able to communicate
+	val akkaHttp = "10.0.9" // keep in sync with akka
+	val apacheCommonsLang3Version = "3.4"
 	val avro = "1.7.7"
 	val camelWebsocket = "2.15.3"
 	val camelElasticSearch = "2.15.3"
@@ -15,18 +12,19 @@ object Versions {
 	val camelQuartz2 = "2.15.3"
 	val elasticSearch = "1.7.2"
 	val elasticSearchSpark = "2.3.3"
+	val hbase = "1.2.0-cdh5.9.1"
+	val jdk = "1.7"
 	val jodaConvert = "1.8.1"
 	val jodaTime = "2.8.2"
 	val json4s = "3.2.10"
 	val kafka = "0.8.2.1"
 	val kryo = "3.0.0"
-	val logback = "1.0.13"
+	val play = "2.4.8"
 	val reactiveMongo = "0.11.14"
+	val scala = "2.11.11"
 	val scalaTest = "2.2.5"
 	val slf4j = "1.7.12"
-	val spark = "1.6.2"
 	val solr = "4.10.4"
 	val solrSpark = "1.0.0"
-	val hbaseSpark = "1.2.0-cdh5.9.1"
-	val apacheCommonsLang3Version = "3.4"
+	val spark = "1.6.2"
 }
