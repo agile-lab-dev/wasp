d7a8d4668ebf30d054164c9797492f289eb9dd42
Mattia Bertorello <mattia.bertorello@agilelab.it>
2017-08-10 17:41:53 +0200

    Fixed routes

--- master/src/main/scala/it/agilelab/bigdata/wasp/web/RestApiRunner.scala
+++ master/src/main/scala/it/agilelab/bigdata/wasp/web/RestApiRunner.scala
@@ -1,33 +1,23 @@
 package it.agilelab.bigdata.wasp.web
 
-import java.util.concurrent.TimeUnit
-
-import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{ExceptionHandler, Route}
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.ActorMaterializer
 import it.agilelab.bigdata.wasp.core.WaspSystem
 import it.agilelab.bigdata.wasp.core.launcher.WaspLauncher
 import it.agilelab.bigdata.wasp.core.logging.WaspLogger
-import it.agilelab.bigdata.wasp.web.controllers.{Index_C, Pipegraph_C, Topic_C}
-import org.apache.commons.lang3.exception.ExceptionUtils
-import spray.json.{JsObject, JsString}
-import akka.http.scaladsl.unmarshalling._
-import akka.stream.scaladsl.Flow
-import akka.util.{ByteString, Timeout}
+import it.agilelab.bigdata.wasp.web.controllers._
 import it.agilelab.bigdata.wasp.web.utils.JsonResultsHelper
-
-import scala.concurrent.{Await, ExecutionContext}
+import org.apache.commons.lang3.exception.ExceptionUtils
 /**
   * Created by Agile Lab s.r.l. on 04/08/2017.
   */
 class RestApiRunner {
-  val logger = WaspLogger(RestApiRunner.getClass.getName)
+  val logger = WaspLogger(getClass.getName)
 
   val myExceptionHandler = ExceptionHandler {
     case e: Exception =>
@@ -48,17 +38,16 @@ class RestApiRunner {
   }
 }
 
-object RestApiRunner {
-  def main(args: Array[String]): Unit = {
-
-
-  }
-}
 
 object RestApiRunnerLauncher extends WaspLauncher {
 
   override protected def startApp(args: Array[String]): Unit = {
-    new RestApiRunner().start(WaspSystem.actorSystem,  Index_C.getRoute ~ Topic_C.getRoute ~ Pipegraph_C.getRoute)
+    new RestApiRunner().start(WaspSystem.actorSystem,
+      BatchJob_C.getRoute ~ Configuration_C.getRoute ~
+        Index_C.getRoute ~ MlModels_C.getRoute  ~
+        Pipegraph_C.getRoute ~ Producer_C.getRoute ~
+        Topic_C.getRoute
+    )
   }
   /**
     * Launchers must override this with deployment-specific pipegraph initialization logic;
