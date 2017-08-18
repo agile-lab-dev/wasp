df9fc7244c8e69d2d2dadcdff00344beeea1560a
Mattia Bertorello <mattia.bertorello@agilelab.it>
2017-08-18 17:07:53 +0200

    Fix current directory in the start-wasp.sh

--- scripts/start-wasp.sh
+++ scripts/start-wasp.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 # absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
-# absolute path this script is in. /home/user/bin
-SCRIPT_DIR=`dirname $SCRIPT`
-# cd into the project root
-cd ${SCRIPT_DIR}/..
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+# This variable contains the directory of the script
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $SCRIPT_DIR/..
 
 # define colors
 red=$(tput setaf 1)
