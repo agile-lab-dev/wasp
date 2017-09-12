f56b3fa9a1340c4dfbe7695c3ab7d25b387e1f5b
Nicolò Bidotti <nicolo.bidotti@agilelab.it>
2017-09-12 15:29:31 +0200

    Improvements to start script
    - less psychdelic output
    - exit on errors

--- scripts/start-wasp.sh
+++ scripts/start-wasp.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# exit on any error
+set -e
+
 # absolute path to this script. /home/user/bin/foo.sh
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -12,11 +15,11 @@ SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd $SCRIPT_DIR/..
 
 # define colors
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-blue=$(tput setaf 4)
-default=$(tput sgr0)
+r=$(tput setaf 1) # red
+g=$(tput setaf 2) # green
+y=$(tput setaf 3) # yellow
+b=$(tput setaf 4) # blue
+d=$(tput sgr0) # default
 
 # kill everything on exit
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
@@ -26,10 +29,10 @@ sbt stage
 
 # launch each module
 LOG4J2_OPTS="-Dlog4j.configurationFile=$SCRIPT_DIR/log4j2.properties"
-master/target/universal/stage/bin/wasp-master ${LOG4J2_OPTS} 2>&1 | sed "s/.*/$red&$default/" &
-producers/target/universal/stage/bin/wasp-producers ${LOG4J2_OPTS} 2>&1 | sed "s/.*/$green&$default/" &
-consumers-rt/target/universal/stage/bin/wasp-consumers-rt ${LOG4J2_OPTS} 2>&1 | sed "s/.*/$yellow&$default/" &
-consumers-spark/target/universal/stage/bin/wasp-consumers-spark ${LOG4J2_OPTS} 2>&1 | sed "s/.*/$blue&$default/" &
+master/target/universal/stage/bin/wasp-master ${LOG4J2_OPTS} 2>&1                   | sed "s/.*/$r master           |$d &/" &
+producers/target/universal/stage/bin/wasp-producers ${LOG4J2_OPTS} 2>&1             | sed "s/.*/$g producers        |$d &/" &
+consumers-rt/target/universal/stage/bin/wasp-consumers-rt ${LOG4J2_OPTS} 2>&1       | sed "s/.*/$y consumers-rt     |$d &/" &
+consumers-spark/target/universal/stage/bin/wasp-consumers-spark ${LOG4J2_OPTS} 2>&1 | sed "s/.*/$b consumers-spark  |$d &/" &
 
 # wait for all children to end
 wait
