56ab8011ab675f9513872bda452d98752ee90f5d
Nicolò Bidotti <nicolo.bidotti@agilelab.it>
2017-09-27 15:43:14 +0200

    Improve dockerized modules stop script

--- docker/stop-wasp.sh
+++ docker/stop-wasp.sh
@@ -7,8 +7,13 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-# This variable contains the directory of the script
+# this variable contains the directory of the script
 SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd $SCRIPT_DIR/..
 
-docker stop master  producers  consumers-rt consumers-spark
+# get docker command
+cd $SCRIPT_DIR
+source get-docker-cmd.sh
+
+# stop containers
+echo "Stopping containers running the modules..."
+$DOCKER_CMD stop master producers consumers-rt consumers-spark
