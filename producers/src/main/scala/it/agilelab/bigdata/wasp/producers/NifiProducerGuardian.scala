de0b8b78f0676921f53c96ecd33887b4f2f40ab4
Alessandro Marino <marino.alemar@gmail.com>
2017-09-27 19:00:14 +0200

    Fixed NifiProducerGuardian and configuration in NifiProducerModel

--- producers/src/main/scala/it/agilelab/bigdata/wasp/producers/NifiProducerGuardian.scala
+++ producers/src/main/scala/it/agilelab/bigdata/wasp/producers/NifiProducerGuardian.scala
@@ -2,31 +2,32 @@ package it.agilelab.bigdata.wasp.producers
 
 import akka.actor.{Actor, ActorRef, PoisonPill, Props}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.headers.RawHeader
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.{ContentTypes, HttpMethods, HttpRequest, HttpResponse}
 import akka.routing.BalancingPool
+import akka.stream.ActorMaterializer
 import it.agilelab.bigdata.wasp.core.WaspSystem.{??, actorSystem}
 import it.agilelab.bigdata.wasp.core.bl.{ProducerBL, TopicBL}
-import it.agilelab.bigdata.wasp.core.cluster.ClusterAwareNodeGuardian
 import it.agilelab.bigdata.wasp.core.kafka.CheckOrCreateTopic
 import it.agilelab.bigdata.wasp.core.logging.Logging
 import it.agilelab.bigdata.wasp.core.models.{ProducerModel, TopicModel}
 import it.agilelab.bigdata.wasp.core.utils.ConfigManager
 import it.agilelab.bigdata.wasp.core.WaspSystem
 import it.agilelab.bigdata.wasp.core.messages.{Start, Stop}
-import org.mongodb.scala.bson.BsonString
 import spray.json._
-
+import DefaultJsonProtocol._
 import scala.concurrent.Future
 
-abstract class NifiProducerGuardian(env: {val producerBL: ProducerBL; val topicBL: TopicBL}, producerId: String)
-  extends ClusterAwareNodeGuardian
+class NifiProducerGuardian(env: {val producerBL: ProducerBL; val topicBL: TopicBL}, producerId: String)
+  extends Actor
     with Logging {
 
-  val name: String
+  val name: String = "NifiProducerGuardian"
+  implicit val materializer = ActorMaterializer()(WaspSystem.actorSystem)
+
   var nifiProducerConf: Option[ProducerModel] = env.producerBL.getById(producerId)
-  val processGroupId: Option[BsonString] = getProcessGroupId("ID") // TODO: Set correct ProcessGroup ID
-  val url = s"server02.cluster01.atscom.it:7070/nifi" // TODO: Set correct nifi-server address
+
+  val processGroupId: Option[String] = getProcessGroupId(nifiProducerConf)
+  val url = s"http://localhost:8084/nifi-api/flow/process-groups/c257f39a-015e-1000-c58b-ec0b29141625"
 
   // initialized in initialize()
   var producer: ProducerModel = _
@@ -34,24 +35,13 @@ abstract class NifiProducerGuardian(env: {val producerBL: ProducerBL; val topicB
   var router_name: String = _
   var kafka_router: ActorRef = _ // TODO: Careful with kafka router dynamic name
 
-  override def postStop(): Unit = {
-    super.postStop()
-    kafka_router ! PoisonPill
-  }
-
-  override def preStart(): Unit = {
-    super.preStart()
-  }
-
-  override def uninitialized: Actor.Receive = super.uninitialized
-
-  override def initialized: Actor.Receive = {
+  override def receive: Actor.Receive = {
 
     case Start =>
       logger.info(s"Producer $producerId starting at guardian $self")
 
       if(processGroupId.isDefined) {
-        val responseFuture = get(url, requestType(processGroupId.toString, "RUNNING"))
+        get(url, requestType(processGroupId.get, "RUNNING"))
         sender() ! true
       }
 
@@ -59,12 +49,12 @@ abstract class NifiProducerGuardian(env: {val producerBL: ProducerBL; val topicB
       logger.info(s"Producer $producerId stopping")
 
       if(processGroupId.isDefined) {
-        val responseFuture = get(url, requestType(processGroupId.toString, "STOPPED"))
+        get(url, requestType(processGroupId.get, "STOPPED"))
         sender() ! true
       }
   }
 
-  override def initialize(): Unit = {
+  def initialize(): Unit = {
 
     val producerOption = env.producerBL.getById(producerId)
 
@@ -77,8 +67,6 @@ abstract class NifiProducerGuardian(env: {val producerBL: ProducerBL; val topicB
         if (??[Boolean](WaspSystem.kafkaAdminActor, CheckOrCreateTopic(topicOption.get.name, topicOption.get.partitions, topicOption.get.replicas))) {
           router_name = s"kafka-ingestion-router-$name-${producer._id.get.getValue.toHexString}-${System.currentTimeMillis()}"
           kafka_router = actorSystem.actorOf(BalancingPool(5).props(Props(new KafkaPublisherActor(ConfigManager.getKafkaConfig))), router_name)
-          context become initialized
-          //startChildActors()
 
           env.producerBL.setIsActive(producer, isActive = true)
         } else {
@@ -93,27 +81,21 @@ abstract class NifiProducerGuardian(env: {val producerBL: ProducerBL; val topicB
   }
 
   // Get ProcessGroup ID in BsonString from id of the nifiProducerConf
-  def getProcessGroupId(id: String): Option[BsonString] = {
-    nifiProducerConf
-      .flatMap(
-        elem => elem.configuration
-          .map(conf => conf.getString(id)))
+  def getProcessGroupId(producerConf: Option[ProducerModel]): Option[String] = {
+    if (!producerConf.isDefined) None
+    producerConf.get.configuration
   }
 
   // Get Future[HttpResponse] representing http-request
-  def get(url: String, jsonReq: Unit): Future[HttpResponse] = {
+  def get(url: String, jsonReq: JsValue): Future[HttpResponse] = {
     Http().singleRequest(
       HttpRequest(uri = url)
-        .withHeaders(RawHeader("Accept", "application/json"))
-        .withHeaders(RawHeader("Content-Type", "application/json"))
-        .withEntity(jsonReq.toString)
+        .withMethod(HttpMethods.PUT)
+        .withEntity(ContentTypes.`application/json`, jsonReq.toString())
     )
   }
 
   // Set request-type (RUNNING or STOPPED)
-  def requestType(id: String, reqType: String): Unit = {
-    NifiStatus(id, reqType, "").toJson
-  }
+  def requestType(id: String, reqType: String): JsValue =
+    Map("id" -> id, "state" -> reqType).toJson
 }
-
-case class NifiStatus(id: String, state: String, components: String)
--- producers/src/main/scala/it/agilelab/bigdata/wasp/producers/NifiProducerModel.scala
+++ producers/src/main/scala/it/agilelab/bigdata/wasp/producers/NifiProducerModel.scala
@@ -3,14 +3,16 @@ package it.agilelab.bigdata.wasp.producers
 import it.agilelab.bigdata.wasp.core.models.ProducerModel
 import org.mongodb.scala.bson.{BsonDocument, BsonObjectId, BsonString}
 
+import scala.util.parsing.json.JSON
+
 object NifiProducerModel {
 
   lazy val nifiProducer = ProducerModel(
-    name = "NifiProducer",
-    className = "it.agilelab.bigdata.wasp.master.NifiProducer",
+    name = "NifiProducerGuardian",
+    className = "it.agilelab.bigdata.wasp.producers.NifiProducerGuardian",
     id_topic = None,
     isActive = false,
-    configuration = Some(BsonDocument("id" -> "value")), // TODO: Set correct ProcessGroup ID
+    configuration = Some("c257f39a-015e-1000-c58b-ec0b29141625"),
     isRemote = false,
     isSystem = false,
     _id = Some(BsonObjectId())
