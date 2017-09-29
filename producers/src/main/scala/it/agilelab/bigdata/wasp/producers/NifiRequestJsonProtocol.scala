b8bb3bb121f1d9d8306e1b403f914d0321d49c4c
Alessandro Marino <marino.alemar@gmail.com>
2017-09-29 18:09:07 +0200

    Added NifiRequestJsonProtocol.

--- producers/src/main/scala/it/agilelab/bigdata/wasp/producers/NifiRequestJsonProtocol.scala
+++ producers/src/main/scala/it/agilelab/bigdata/wasp/producers/NifiRequestJsonProtocol.scala
@@ -1,5 +1,13 @@
 package it.agilelab.bigdata.wasp.producers
 
-class NifiRequestJsonProtocol {
+import spray.json.DefaultJsonProtocol
 
+object NifiRquestJsonProtocol extends DefaultJsonProtocol {
+  implicit def nifiPlatform = jsonFormat2(NifiPlatform.apply)
+  implicit def nifiRequest = jsonFormat3(NifiRequest.apply)
+  implicit def nifiConfiguration = jsonFormat2(Configuration.apply)
 }
+
+case class NifiRequest(action: String, id: Option[String], child: Option[List[NifiPlatform]])
+case class NifiPlatform(id: String, edge: Option[List[String]])
+case class Configuration(routingInfo: NifiRequest, data: Option[Array[Byte]])
