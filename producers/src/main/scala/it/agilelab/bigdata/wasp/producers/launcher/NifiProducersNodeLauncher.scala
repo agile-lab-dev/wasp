c0d4bd731fa57f4a455b196311a44e4770d8a080
Alessandro Marino <marino.alemar@gmail.com>
2017-10-03 12:36:05 +0200

    Added InsertModelLauncher object for the insertion of machine-learning models.

--- producers/src/main/scala/it/agilelab/bigdata/wasp/producers/launcher/NifiProducersNodeLauncher.scala
+++ producers/src/main/scala/it/agilelab/bigdata/wasp/producers/launcher/NifiProducersNodeLauncher.scala
@@ -1,7 +1,12 @@
 package it.agilelab.bigdata.wasp.producers.launcher
 
-import it.agilelab.bigdata.wasp.core.models.ProducerModel
+import java.io.File
+
+import it.agilelab.bigdata.wasp.core.models.{MlModelOnlyInfo, ProducerModel}
 import it.agilelab.bigdata.wasp.producers.NifiProducerModel
+import org.apache.commons.io.FileUtils
+import org.apache.commons.lang.SerializationUtils
+import org.bson.BsonDocument
 
 /**
   * Launcher for the NifiProducer.
@@ -9,9 +14,23 @@ import it.agilelab.bigdata.wasp.producers.NifiProducerModel
   * @author Alessandro Marino
   */
 
+case class modelFile(data: Array[Byte], fileName: String, metadata: BsonDocument)
+
 object NifiProducersNodeLauncher extends ProducersNodeLauncherTrait {
   override def launch(args: Array[String]): Unit = {
     super.launch(args)
     waspDB.insertIfNotExists[ProducerModel](NifiProducerModel.nifiProducer)
   }
+}
+
+object InsertModelLauncher extends ProducersNodeLauncherTrait {
+  override def launch(args: Array[String]): Unit = {
+    super.launch(args)
+    val fileName = "/home/amarino/Agile.Wasp2/RegressionSumModels_1"
+
+    val data = new File(fileName)
+    val vec: Array[Byte] = FileUtils.readFileToByteArray(data)
+    val res = waspDB.saveFile(vec, fileName, new BsonDocument())
+  }
+
 }
