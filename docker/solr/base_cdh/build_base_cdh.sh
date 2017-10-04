71e1d3169196356a0e5d53f732724722a6f8b154
Mattia Bertorello <mattia.bertorello@agilelab.it>
2017-10-04 19:21:18 +0200

    Added Solr container

---
+++ docker/solr/base_cdh/build_base_cdh.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build --rm -t base_cdh:5.11 .
---
+++ docker/solr/build_solr_cdh.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# exit on any error
+set -e
+
+# absolute path to this script. /home/user/bin/foo.sh
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+# this variable contains the directory of the script
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+source $SCRIPT_DIR/../get-docker-cmd.sh
+
+cd base_cdh
+
+$DOCKER_CMD build --rm -t base_cdh:5.11 .
+
+cd ..
+
+$DOCKER_CMD build --rm -t solr_cdh:5.11 .
