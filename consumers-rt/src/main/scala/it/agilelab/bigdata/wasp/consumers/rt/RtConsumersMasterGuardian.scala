7457543520ba693c6c0d096639fd24906aa5a91f
Nicolò Bidotti <nicolo.bidotti@agilelab.it>
2017-10-23 18:44:59 +0200

    Improve RtConsumersMasterGuardian start/stop sequences
    - improve sequences
    - make startup pipegaph-based rather than component-based
    - add actor tracking

--- consumers-rt/src/main/scala/it/agilelab/bigdata/wasp/consumers/rt/RtConsumersMasterGuardian.scala
+++ consumers-rt/src/main/scala/it/agilelab/bigdata/wasp/consumers/rt/RtConsumersMasterGuardian.scala
@@ -5,6 +5,7 @@ import akka.pattern.gracefulStop
 import it.agilelab.bigdata.wasp.core.WaspSystem.generalTimeout
 import it.agilelab.bigdata.wasp.core.bl._
 import it.agilelab.bigdata.wasp.core.consumers.BaseConsumersMasterGuadian
+import it.agilelab.bigdata.wasp.core.consumers.BaseConsumersMasterGuadian.generateUniqueComponentName
 import it.agilelab.bigdata.wasp.core.models.RTModel
 import it.agilelab.bigdata.wasp.core.utils.WaspConfiguration
 
@@ -32,33 +33,72 @@ class RtConsumersMasterGuardian(env: {
   
   // methods implementing start/stop ===================================================================================
   
+  
+  
   override def beginStartup(): Unit = {
     logger.info(s"RtConsumersMasterGuardian $self beginning startup sequence...")
+  
+    // gab map containing pipegraph -> components info for active pipegraphs
+    logger.info(s"Loading all active pipegraphs...")
+    val pipegraphsToComponentsMap = getActivePipegraphsToComponentsMap
+    logger.info(s"Found ${pipegraphsToComponentsMap.size} active pipegraphs")
+  
+    // zero counters for components
+    rtTotal = 0
+  
+    // update counters for components
+    pipegraphsToComponentsMap foreach {
+      case (pipegraph, (lseComponents, sseComponents, rtComponents)) => {
+        // grab sizes
+        val rtComponentsListSize = rtComponents.size
+      
+        // increment size counters for components
+        rtTotal += rtComponentsListSize
+      
+        logger.info(s"Found $rtComponentsListSize total rt components for pipegraph ${pipegraph.name}")
+      }
+    }
+  
+    logger.info(s"Found $rtTotal total rt components")
+  
+    if (rtTotal == 0) { // no active pipegraphs/no components to start
+      logger.info("No active pipegraphs with rt components found; aborting startup sequence")
     
-    // find all rt components marked as active, save how many they are in state
-    val activeRtComponents = findActiveRtComponents
-    rtTotal = activeRtComponents.size
-    
-    if (activeRtComponents.isEmpty) {
-      // no rts to start
-      logger.info(s"Found $rtTotal RT components marked as active; entering uninitialized state")
+      // enter unitizialized state because we don't have anything to do
       context become uninitialized
-      logger.info("Sending success to master guardian")
+      logger.info(s"RtConsumersMasterGuardian $self is now in uninitialized state")
+    
+      // answer ok to MasterGuardian since this is normal if all pipegraphs are unactive
       masterGuardian ! true
-      logger.info("Startup sequence completed, no RT components started")
-    } else {
-      // start rts
-      logger.info(s"Found $rtTotal RT components marked as active; entering starting state")
+    } else { // we have pipegaphs/components to start
+      // enter starting state so we stash restarts
       context become starting
-      logger.info("Starting RT components...")
-      activeRtComponents foreach {
-        rtComponent => {
-          logger.info(s"Starting RT actor for component ${rtComponent.name}")
-          val rtActor = context.actorOf(Props(new ConsumerRTActor(env, rtComponent, self)))
-          rtActor ! StartRT
-          logger.info(s"Started RT actor for component ${rtComponent.name}")
+      logger.info(s"RtConsumersMasterGuardian $self is now in starting state")
+    
+      // loop over pipegraph -> components map spawning the appropriate actors for each component
+      pipegraphsToComponentsMap foreach {
+        case (pipegraph, (lseComponents, sseComponents, rtComponents)) => {
+          logger.info(s"Starting component actors for pipegraph ${pipegraph.name}")
+        
+          // start actors for rt components
+          rtComponents.foreach(component => {
+            // start component actor
+            logger.info(s"Starting RTActor for pipegraph ${pipegraph.name}, component ${component.name}...")
+            val actor = context.actorOf(Props(new ConsumerRTActor(env, component, self)))
+            actor ! StartRT
+            logger.info(s"Started LegacyStreamingETLActor $actor for pipegraph ${pipegraph.name}, component ${component.name}")
+          
+            // add to component actor tracking map
+            rtComponentActors += (generateUniqueComponentName(pipegraph, component) -> actor)
+          })
+        
+          // do not do anything for legacy streaming/structured streaming components
+          logger.info(s"Ignoring ${lseComponents.size} legacy streaming components and ${sseComponents.size} structured " +
+                      s"streaming components for pipegraph ${pipegraph.name} as they are handled by " +
+                      s"SparkConsumersMasterGuardian")
         }
       }
+      
       // all component actors started; now we wait for them to send us back all the OutputStreamInitialized messages
       logger.info(s"RtConsumersMasterGuardian $self pausing startup sequence, waiting for all component actors to register...")
     }
@@ -81,22 +121,63 @@ class RtConsumersMasterGuardian(env: {
   
   override def stop(): Boolean = {
     logger.info(s"Stopping...")
-    
-    // stop all actors bound to this guardian and the guardian itself
-    logger.info(s"Stopping child actors bound to this rt consumers master guardian $self")
+  
+    // stop all component actors bound to this guardian and the guardian itself
+    logger.info(s"Stopping component actors bound to RtConsumersMasterGuardian $self...")
+  
+    // find and stop all RTActors belonging to pipegraphs that are no longer active
+    // get the component names for all components of all active pipegraphs
+    val activeRTComponentNames = getActivePipegraphsToComponentsMap flatMap {
+      case (pipegraph, (_, _, rtComponents)) => {
+        rtComponents map { component => generateUniqueComponentName(pipegraph, component) }
+      }
+    } toSet
+    // diff with the set of component names for component actors to find the ones we have to stop
+    val inactiveRTComponentNames = rtComponentActors.keys.toSet.diff(activeRTComponentNames)
+    // grab corresponding actor refs
+    val inactiveRTComponentActors = inactiveRTComponentNames.map(rtComponentActors).toSeq
+    // gracefully stop all component actors corresponding to now-inactive pipegraphs
+    logger.info(s"Gracefully stopping ${inactiveRTComponentActors.size} rt component actors managing now-inactive components...")
     val generalTimeoutDuration = generalTimeout.duration
-    val globalStatus = Future.traverse(context.children)(gracefulStop(_, generalTimeoutDuration))
-    val res = Await.result(globalStatus, generalTimeoutDuration)
-
+    val rtStatuses = inactiveRTComponentActors.map(gracefulStop(_, generalTimeoutDuration))
+  
+    // await all component actors' stopping
+    val res = Await.result(Future.sequence(rtStatuses), generalTimeoutDuration)
+  
+    // check whether all components actors that had to stop actually stopped
     if (res reduceLeft (_ && _)) {
       logger.info(s"Stopping sequence completed")
-      numberOfReadyComponents = 0
+    
+      // cleanup references to now stopped component actors
+      inactiveRTComponentNames.map(rtComponentActors.remove) // remove rt components actors that we stopped
+    
+      // update counter for ready components because some rt components actors might have been left running
+      numberOfReadyComponents = rtComponentActors.size
+    
+      // no message sent to the MasterGuardian because we still need to startup again after this
+    
       true
     } else {
-      logger.error(s"Stopping sequence failed! Unable to shutdown all children")
-      numberOfReadyComponents = context.children.size
+      logger.error(s"Stopping sequence failed! Unable to shutdown all components actors")
+    
+      // find out which children are still running
+      val childrenSet = context.children.toSet
+      numberOfReadyComponents = childrenSet.size
       logger.error(s"Found $numberOfReadyComponents children still running")
+    
+      // filter out component actor tracking maps
+      val filteredRTCA = rtComponentActors filter { case (name, actor) => childrenSet(actor) }
+      rtComponentActors.clear()
+      rtComponentActors ++= filteredRTCA
+    
+      // output info about component actors still running
+      rtComponentActors foreach {
+        case (name, actor) => logger.error(s"RT component actor $actor for component $name is still running")
+      }
+    
+      // tell the MasterGuardian we failed
       masterGuardian ! false
+    
       false
     }
   }
