196beb7550f1f939b1517ac5b4acb02ca856d802
Nicolò Bidotti <nicolo.bidotti@agilelab.it>
2017-10-23 17:35:13 +0200

    Overhaul RtConsumersMasterGuardian
    - restructure to follow SparkConsumersMasterGuardian
    (beginStartup/registerComponentActor/finishStartup/stop)
    - remove inheritance from ClusterAwareNodeGuardian
    - move initial behaviour switch to uninitialized to preStart()

--- consumers-rt/src/main/scala/it/agilelab/bigdata/wasp/consumers/rt/RtConsumersMasterGuardian.scala
+++ consumers-rt/src/main/scala/it/agilelab/bigdata/wasp/consumers/rt/RtConsumersMasterGuardian.scala
@@ -5,12 +5,12 @@ import akka.pattern.gracefulStop
 import it.agilelab.bigdata.wasp.core.WaspEvent.OutputStreamInitialized
 import it.agilelab.bigdata.wasp.core.WaspSystem.generalTimeout
 import it.agilelab.bigdata.wasp.core.bl._
-import it.agilelab.bigdata.wasp.core.cluster.ClusterAwareNodeGuardian
 import it.agilelab.bigdata.wasp.core.logging.Logging
 import it.agilelab.bigdata.wasp.core.messages.RestartConsumers
 import it.agilelab.bigdata.wasp.core.models.RTModel
 import it.agilelab.bigdata.wasp.core.utils.WaspConfiguration
 
+import scala.collection.mutable
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -21,117 +21,96 @@ class RtConsumersMasterGuardian(env: {
                                        val indexBL: IndexBL
                                        val websocketBL: WebsocketBL
                                      })
-    extends ClusterAwareNodeGuardian
-    with Stash
+    extends Stash
     with Logging
     with WaspConfiguration {
+  // counters for components
+  private var rtTotal = 0
   
-  // initial state is uninitialized
-  context become uninitialized
-
-  // internal state
-  private var lastRestartMasterRef: ActorRef = _ // last master actor ref, needed for ask pattern
-  private var numActiveRtComponents = 0 // number of currently active rt components (i.e. running)
-  private var targetNumActiveRtComponents = 0 // target number of active rt components (i.e. should be running)
-
-  // available states ==================================================================================================
+  // counter for ready components
+  private var numberOfReadyComponents = 0
   
-  override def uninitialized: Actor.Receive = {
-    case RestartConsumers =>
-      lastRestartMasterRef = sender()
-      startGuardian()
+  // tracking map for rt components ( componentName -> RTActor )
+  private val rtComponentActors: mutable.Map[String, ActorRef] = mutable.Map.empty[String, ActorRef]
+  
+  // ActorRef to MasterGuardian returned by the last ask - cannot be replaced with a simple ActorRef or singleton proxy!
+  private var masterGuardian: ActorRef = _
+  
+  // actor lifecycle callbacks =========================================================================================
+  
+  override def preStart(): Unit = {
+    // we start in uninitialized state
+    context become uninitialized
   }
-
+  
+  // behaviours ========================================================================================================
+  
+  // standard receive
+  // NOTE: THIS IS IMMEDIATELY SWITCHED TO uninitialized DURING preStart(), DO NOT USE!
+  override def receive: Actor.Receive = uninitialized
+  
+  // behaviour when uninitialized
+  def uninitialized: Actor.Receive = {
+    case RestartConsumers =>
+      // update MasterGuardian ActorRef
+      masterGuardian = sender()
+      
+      beginStartup()
+  }
+  
+  // behaviour while starting
   def starting: Actor.Receive = {
     case OutputStreamInitialized =>
-      logger.info(s"RT component ${sender()} confirmed it is running")
-      numActiveRtComponents += 1
-      initialize()
+      // register component actor
+      registerComponentActor(sender())
+      
+      if (numberOfReadyComponents == rtTotal) {
+        // all component actors registered; finish startup
+        logger.info(s"All consumer $numberOfReadyComponents child actors have registered! Continuing startup sequence...")
+        finishStartup()
+      } else {
+        logger.info(s"Not all component actors have registered to the cluster (right now only $numberOfReadyComponents " +
+                      s"out of $rtTotal), waiting for more...")
+      }
     case RestartConsumers =>
-      logger.info(s"Stashing restart ...")
+      logger.info(s"Stashing RestartConsumers from ${sender()}")
       stash()
   }
-
-  /** This node guardian's customer behavior once initialized. */
-  override def initialized: Actor.Receive = {
+  
+  // behavior once initialized
+  def initialized: Actor.Receive = {
     case RestartConsumers =>
-      lastRestartMasterRef = sender()
-      val stopSuccessful = stopGuardian()
-      if (stopSuccessful) { // restart only if stopping was successful
-        startGuardian()
+      // update MasterGuardian ActorRef
+      masterGuardian = sender()
+      
+      // attempt stopping
+      val stopSuccessful = stop()
+  
+      // only proceed with restart if we actually stopped
+      if (stopSuccessful) {
+        beginStartup()
       }
   }
   
-  // overriden ClusterAwareNodeGuardian methods ========================================================================
+  // methods implementing start/stop ===================================================================================
   
-  // TODO do we really need joinSeedNodes here? we are we trying to join ourselves???
-  override def preStart(): Unit = {
-    super.preStart()
-    //TODO:capire joinseednodes
-    cluster.joinSeedNodes(Vector(cluster.selfAddress))
-  }
-  
-  // TODO should we really be calling this multiple times? maybe it should be "attemptInitialization()"?
-  override def initialize(): Unit = {
-    super.initialize()
-  
-    logger.info(s"Currently $numActiveRtComponents out of $targetNumActiveRtComponents active RT components are " +
-                 "confirmed as running")
-    
-    if (numActiveRtComponents == targetNumActiveRtComponents) {
-      logger.info("All active RT components are confirmed as running; entering initialized state")
-      context become initialized
-      logger.info("Sending success to master guardian")
-      lastRestartMasterRef ! true
-      logger.info(s"Startup sequence completed, all $targetNumActiveRtComponents RT components are running")
-      logger.info("Unstashing queued messages...")
-      unstashAll()
-    } else {
-      logger.info("Not all active RT components are confirmed as running; waiting for the remaining ones")
-    }
-  }
-
-  // private methods ===================================================================================================
-
-  private def stopGuardian(): Boolean = {
-    logger.info(s"Stopping...")
-    
-    // stop all actors bound to this guardian and the guardian itself
-    logger.info(s"Stopping child actors bound to this rt consumers master guardian $self")
-    val generalTimeoutDuration = generalTimeout.duration
-    val globalStatus = Future.traverse(context.children)(gracefulStop(_, generalTimeoutDuration))
-    val res = Await.result(globalStatus, generalTimeoutDuration)
-
-    if (res reduceLeft (_ && _)) {
-      logger.info(s"Stopping sequence completed")
-      numActiveRtComponents = 0
-      true
-    } else {
-      logger.error(s"Stopping sequence failed! Unable to shutdown all children")
-      numActiveRtComponents = context.children.size
-      logger.error(s"Found $numActiveRtComponents children still running")
-      lastRestartMasterRef ! false
-      false
-    }
-  }
-
-  private def startGuardian(): Unit = {
-    logger.info(s"Starting up...")
+  private def beginStartup(): Unit = {
+    logger.info(s"RtConsumersMasterGuardian $self beginning startup sequence...")
     
     // find all rt components marked as active, save how many they are in state
     val activeRtComponents = findActiveRtComponents
-    targetNumActiveRtComponents = activeRtComponents.size
+    rtTotal = activeRtComponents.size
     
     if (activeRtComponents.isEmpty) {
       // no rts to start
-      logger.info(s"Found $targetNumActiveRtComponents RT components marked as active; entering uninitialized state")
+      logger.info(s"Found $rtTotal RT components marked as active; entering uninitialized state")
       context become uninitialized
       logger.info("Sending success to master guardian")
-      lastRestartMasterRef ! true
+      masterGuardian ! true
       logger.info("Startup sequence completed, no RT components started")
     } else {
       // start rts
-      logger.info(s"Found $targetNumActiveRtComponents RT components marked as active; entering starting state")
+      logger.info(s"Found $rtTotal RT components marked as active; entering starting state")
       context become starting
       logger.info("Starting RT components...")
       activeRtComponents foreach {
@@ -142,7 +121,50 @@ class RtConsumersMasterGuardian(env: {
           logger.info(s"Started RT actor for component ${rtComponent.name}")
         }
       }
-      logger.info(s"Started $targetNumActiveRtComponents RT components; waiting for confirmation that they are all running")
+      // all component actors started; now we wait for them to send us back all the OutputStreamInitialized messages
+      logger.info(s"RtConsumersMasterGuardian $self pausing startup sequence, waiting for all component actors to register...")
+    }
+  }
+  
+  private def registerComponentActor(componentActor: ActorRef): Unit = {
+    logger.info(s"Component actor $componentActor registered")
+    numberOfReadyComponents += 1
+  }
+  
+  private def finishStartup(): Unit = {
+    logger.info(s"RtConsumersMasterGuardian $self continuing startup sequence...")
+    
+    // confirm startup success to MasterGuardian
+    masterGuardian ! true
+  
+    // enter intialized state
+    context become initialized
+    logger.info(s"RtConsumersMasterGuardian $self is now in initialized state")
+  
+    // unstash messages stashed while in starting state
+    logger.info("Unstashing queued messages...")
+    unstashAll()
+  }
+  
+  private def stop(): Boolean = {
+    logger.info(s"Stopping...")
+    
+    // stop all actors bound to this guardian and the guardian itself
+    logger.info(s"Stopping child actors bound to this rt consumers master guardian $self")
+    val generalTimeoutDuration = generalTimeout.duration
+    val globalStatus = Future.traverse(context.children)(gracefulStop(_, generalTimeoutDuration))
+    val res = Await.result(globalStatus, generalTimeoutDuration)
+
+    if (res reduceLeft (_ && _)) {
+      logger.info(s"Stopping sequence completed")
+      numberOfReadyComponents = 0
+      true
+    } else {
+      logger.error(s"Stopping sequence failed! Unable to shutdown all children")
+      numberOfReadyComponents = context.children.size
+      logger.error(s"Found $numberOfReadyComponents children still running")
+      masterGuardian ! false
+      false
     }
   }
 
