5078347587e0b6f73905d297cfe9a54aff2f6c75
Davide Colombatto <davide.colombatto@gmail.com>
2018-01-23 19:29:56 +0100

    ISC-322: upd

--- core/src/main/scala/it/agilelab/bigdata/wasp/core/cluster/ClusterListenerActor.scala
+++ core/src/main/scala/it/agilelab/bigdata/wasp/core/cluster/ClusterListenerActor.scala
@@ -23,20 +23,29 @@ object ClusterListenerActor {
 class ClusterListenerActor extends Actor with Logging {
 
   val cluster = Cluster(context.system)
-  var unreachableMembers: Set[Member] = Set()
+  var unreachableMembers: Set[Member] = Set.empty
 
-  // subscribe to cluster changes, re-subscribe when restart
-  override def preStart(): Unit = cluster.subscribe(self, initialStateMode = InitialStateAsEvents, classOf[UnreachableMember], classOf[ReachableMember])
+  // Subscribe to cluster changes, re-subscribe when restart.
+  // N.B. ClusterDomainEvent includes all events (e.g.  MemberEvent, ReachabilityEvent)
+  // TODO: test InitialStateAsEvents vs InitialStateAsSnapshot
+  override def preStart(): Unit = cluster.subscribe(self, initialStateMode = InitialStateAsEvents, classOf[ClusterDomainEvent])
+  //override def preStart(): Unit = cluster.subscribe(self, initialStateMode = InitialStateAsSnapshot, classOf[ClusterDomainEvent])
 
   override def postStop(): Unit = cluster.unsubscribe(self)
 
   def receive: Actor.Receive = {
 
-    case MemberUp(member) => onMemberUp(member)
+    /* MemberEvent */
+    case MemberUp(member) => logger.info(s"Member ${member.address} joined cluster.")
+    case MemberLeft(member) => logger.info(s"Member ${member.address} lefted cluster.")
+    case MemberRemoved(member, previousStatus) => logger.info(s"Member is removed: ${member.address} - previous status: $previousStatus")
+
+    /*  ReachabilityEvent */
     case UnreachableMember(member) => onUnreachableMember(member)
     case ReachableMember(member) => onReachableMember(member)
-    case MemberRemoved(member, previousStatus) => onMemberRemoved(member, previousStatus)
-    case ClusterMetricsChanged(forNode) => onClusterMetricsChanged(forNode)
+
+    /* other ClusterDomainEvent */
+    case ClusterMetricsChanged(nodeMetrics) => onClusterMetricsChanged(nodeMetrics)
     case _: MemberEvent =>  // ignore
 
     case DownUnreachableMembers =>
@@ -46,16 +55,31 @@ class ClusterListenerActor extends Actor with Logging {
           cluster.down(member.address)
         }
       }
+      unreachableMembers.empty
   }
 
-  private def onMemberUp(member: Member) =
-    logger.info(s"Member ${member.address} joined cluster.")
-
   private def onUnreachableMember(member: Member) = {
+
+    def isMajority(total: Int, dead: Int): Boolean = {
+
+      // find out majority number of the group
+      def majority: Int = (total+1)/2 + (total+1)%2
+
+      require(total > 0)
+      require(dead >= 0)
+      (total - dead) >= majority
+    }
+
+    def scheduleTakeDown = {
+      implicit val dispatcher = context.system.dispatcher
+      unreachableMembers = unreachableMembers + member
+      context.system.scheduler.scheduleOnce(ClusterListenerActor.downingTimeout, self, DownUnreachableMembers)
+    }
+
     logger.info(s"Member detected as unreachable: ${member}")
     val state = cluster.state
     if (isMajority(state.members.size, state.unreachable.size))
-      scheduleTakeDown(member)
+      scheduleTakeDown
   }
 
   private def onReachableMember(member: Member) = {
@@ -63,32 +87,15 @@ class ClusterListenerActor extends Actor with Logging {
     unreachableMembers = unreachableMembers - member
   }
 
-  private def onMemberRemoved(member: Member, previousStatus: MemberStatus) =
-    logger.info(s"Member is removed: ${member.address} - previous status: $previousStatus")
+  private def onClusterMetricsChanged(nodeMetrics: Set[NodeMetrics]) {
 
-  private def onClusterMetricsChanged(forNode: Set[NodeMetrics]) {
-    forNode collectFirst {
+    def filter(nodeMetrics: Set[Metric]): String = {
+      val filtered = nodeMetrics collect { case v if v.name != "processors" => s"${v.name}:${v.value}" }
+      s"NodeMetrics[${filtered.mkString(",")}]"
+    }
+
+    nodeMetrics collectFirst {
       case m if m.address == cluster.selfAddress => logger.debug (s"${filter (m.metrics)}")
     }
-  }
-
-  private def filter(nodeMetrics: Set[Metric]): String = {
-    val filtered = nodeMetrics collect { case v if v.name != "processors" => s"${v.name}:${v.value}" }
-    s"NodeMetrics[${filtered.mkString(",")}]"
-  }
-
-  // find out majority number of the group
-  private def majority(n: Int): Int = (n+1)/2 + (n+1)%2
-
-  private def isMajority(total: Int, dead: Int): Boolean = {
-    require(total > 0)
-    require(dead >= 0)
-    (total - dead) >= majority(total)
-  }
-
-  private def scheduleTakeDown(member: Member) = {
-    implicit val dispatcher = context.system.dispatcher
-    unreachableMembers = unreachableMembers + member
-    context.system.scheduler.scheduleOnce(ClusterListenerActor.downingTimeout, self, DownUnreachableMembers)
   }
 }
