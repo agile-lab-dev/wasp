fde048f4ba8089a906a476689c71650210342ae3
Davide Colombatto <davide.colombatto@gmail.com>
2018-03-27 12:58:30 +0200

    cleanup

--- whitelabel/consumers-spark/src/main/scala/it/agilelab/bigdata/wasp/whitelabel/consumers/spark/strategies/test/TestIdentityStrategy.scala
+++ whitelabel/consumers-spark/src/main/scala/it/agilelab/bigdata/wasp/whitelabel/consumers/spark/strategies/test/TestIdentityStrategy.scala
@@ -1,7 +1,6 @@
 package it.agilelab.bigdata.wasp.whitelabel.consumers.spark.strategies.test
 
 import it.agilelab.bigdata.wasp.consumers.spark.strategies.{ReaderKey, Strategy}
-import it.agilelab.bigdata.wasp.core.logging.Logging
 import org.apache.spark.sql.DataFrame
 
 class TestIdentityStrategy extends Strategy {
--- whitelabel/models/src/main/scala/it/agilelab/bigdata/wasp/whitelabel/models/test/TestDocument.scala
+++ whitelabel/models/src/main/scala/it/agilelab/bigdata/wasp/whitelabel/models/test/TestDocument.scala
@@ -1,11 +1,10 @@
 package it.agilelab.bigdata.wasp.whitelabel.models.test
 
-
 case class TestDocument(id: String,
                         number: Int,
                         nested: TestNestedDocument)
 
-
 case class TestNestedDocument(field1: String,
                               field2: Long,
                               field3: Option[String])
+
