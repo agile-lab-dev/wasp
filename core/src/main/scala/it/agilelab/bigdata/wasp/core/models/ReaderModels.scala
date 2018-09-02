363898ae85a565120c7fce12e4078ea740148d26
Nicolò Bidotti <nb.m1lt0n.90@gmail.com>
2018-09-03 01:57:35 +0200

    Add options field

--- core/src/main/scala/it/agilelab/bigdata/wasp/core/models/ReaderModels.scala
+++ core/src/main/scala/it/agilelab/bigdata/wasp/core/models/ReaderModels.scala
@@ -2,33 +2,46 @@ package it.agilelab.bigdata.wasp.core.models
 
 import it.agilelab.bigdata.wasp.core.datastores._
 
+
 // TODO: switch to new apply as main ctor (see companion)
 /**
-	* A model for a reader, composed by a name, an datastoreModelName defining the datastore, and a datastoreProduct
-	* defining the datastore software product to use.
+	* A model for a reader, composed by a name, a datastoreModelName defining the datastore, a datastoreProduct
+	* defining the datastore software product to use, and any additional options needed to configure the reader.
 	*
 	* @param name the name of this reader model
 	* @param datastoreModelName (optional) the name of the endpoint to read from
 	* @param datastoreProduct the datastore software product to be used when reading
+  * @param options additional options for the reader
 	*/
-case class ReaderModel @deprecated("Please use the other apply or the factory methods provided in the companion object as they ensure compatibility between the DatastoreModel and the DatastoreProduct")
-                       (name: String, datastoreModelName: String, datastoreProduct: String) // why do we even normalize and not save the datastore model inside? it's bloody mongodb...
+case class ReaderModel @deprecated("Please use the other apply or the factory methods provided in the companion " +
+	                                 "object as they ensure compatibility between the DatastoreModel and the " +
+                                   "DatastoreProduct")
+                       (name: String, datastoreModelName: String, datastoreProduct: String, options: Map[String, String] = Map.empty)
 
 object ReaderModel {
   // unfortunately we can't use this as the main ctor right now because DatastoreProduct doesn't have a working mongodb
   // codec and we need to write our own
   def apply[DSC <: DatastoreCategory, DSP <: DatastoreProduct]
-           (name: String, datastoreModel: DatastoreModel[DSC], datastoreProduct: DSP)
+           (name: String, datastoreModel: DatastoreModel[DSC], datastoreProduct: DSP, options: Map[String, String] = Map.empty)
            (implicit ev: DSP <:< DSC): ReaderModel = {
 		ReaderModel(name, datastoreModel.name, datastoreProduct.getActualProduct)
 	}
-	def indexReader(name: String, indexModel: IndexModel) = apply(name, indexModel, GenericIndexProduct)
-	def elasticReader(name: String, indexModel: IndexModel) = apply(name, indexModel, ElasticProduct)
-	def solrReader(name: String, indexModel: IndexModel) = apply(name, indexModel, SolrProduct)
-	def keyValueReader(name: String, keyValueModel: KeyValueModel) = apply(name, keyValueModel, GenericKeyValueProduct)
-	def hbaseReader(name: String, keyValueModel: KeyValueModel) = apply(name, keyValueModel, HBaseProduct)
-	def topicReader(name: String, topicModel: TopicModel) = apply(name, topicModel, GenericTopicProduct)
-	def kafkaReader(name: String, topicModel: TopicModel) = apply(name, topicModel, KafkaProduct)
-	def rawReader(name: String, rawModel: RawModel) = apply(name, rawModel, RawProduct)
-	def websocketReader(name: String, websocketModel: WebsocketModel) = apply(name, websocketModel, WebSocketProduct)
+	def indexReader(name: String, indexModel: IndexModel, options: Map[String, String] = Map.empty) =
+    apply(name, indexModel, GenericIndexProduct, options)
+	def elasticReader(name: String, indexModel: IndexModel, options: Map[String, String] = Map.empty) =
+    apply(name, indexModel, ElasticProduct, options)
+	def solrReader(name: String, indexModel: IndexModel, options: Map[String, String] = Map.empty) =
+    apply(name, indexModel, SolrProduct, options)
+	def keyValueReader(name: String, keyValueModel: KeyValueModel, options: Map[String, String] = Map.empty) =
+    apply(name, keyValueModel, GenericKeyValueProduct, options)
+	def hbaseReader(name: String, keyValueModel: KeyValueModel, options: Map[String, String] = Map.empty) =
+    apply(name, keyValueModel, HBaseProduct, options)
+	def topicReader(name: String, topicModel: TopicModel, options: Map[String, String] = Map.empty) =
+    apply(name, topicModel, GenericTopicProduct, options)
+	def kafkaReader(name: String, topicModel: TopicModel, options: Map[String, String] = Map.empty) =
+    apply(name, topicModel, KafkaProduct, options)
+	def rawReader(name: String, rawModel: RawModel, options: Map[String, String] = Map.empty) =
+    apply(name, rawModel, RawProduct, options)
+	def websocketReader(name: String, websocketModel: WebsocketModel, options: Map[String, String] = Map.empty) =
+    apply(name, websocketModel, WebSocketProduct, options)
 }
--- core/src/main/scala/it/agilelab/bigdata/wasp/core/models/WriterModels.scala
+++ core/src/main/scala/it/agilelab/bigdata/wasp/core/models/WriterModels.scala
@@ -5,33 +5,45 @@ import it.agilelab.bigdata.wasp.core.datastores._
 
 // TODO: switch to new apply as main ctor (see companion)
 /**
-	* A model for a writer, composed by a name, an datastoreModelName defining the datastore, and a datastoreProduct
-	* defining the datastore software product to use.
+	* A model for a writer, composed by a name, a datastoreModelName defining the datastore, a datastoreProduct
+	* defining the datastore software product to use, and any additional options needed to configure the writer.
 	*
 	* @param name the name of this writer model
 	* @param datastoreModelName (optional) the name of the endpoint to write to
 	* @param datastoreProduct the datastore software product to be used when writing
+  * @param options additional options for the writer
 	*/
-case class WriterModel @deprecated("Please use the other apply or the factory methods provided in the companion object as they ensure compatibility between the DatastoreModel and the DatastoreProduct")
-                       (name: String, datastoreModelName: Option[String], datastoreProduct: DatastoreProduct) // why do we even normalize and not save the datastore model inside? it's bloody mongodb...
+case class WriterModel @deprecated("Please use the other apply or the factory methods provided in the companion " +
+                                   "object as they ensure compatibility between the DatastoreModel and the " +
+                                   "DatastoreProduct")
+                       (name: String, datastoreModelName: Option[String], datastoreProduct: DatastoreProduct, options: Map[String, String] = Map.empty)
 
 object WriterModel {
   // unfortunately we can't use this as the main ctor right now because DatastoreProduct doesn't have a working mongodb
   // codec and we need to write our own
 	def apply[DSC <: DatastoreCategory, DSP <: DatastoreProduct]
-           (name: String, datastoreModel: DatastoreModel[DSC], datastoreProduct: DSP)
+           (name: String, datastoreModel: DatastoreModel[DSC], datastoreProduct: DSP, options: Map[String, String] = Map.empty)
            (implicit ev: DSP <:< DSC): WriterModel = {
-		WriterModel(name, Some(datastoreModel.name), datastoreProduct)
+		WriterModel(name, Some(datastoreModel.name), datastoreProduct, options)
 	}
-	// helpers to create writer models for supported datastores
-	def indexWriter(name: String, indexModel: IndexModel) = apply(name, indexModel, GenericIndexProduct)
-	def elasticWriter(name: String, indexModel: IndexModel) = apply(name, indexModel, ElasticProduct)
-	def solrWriter(name: String, indexModel: IndexModel) = apply(name, indexModel, SolrProduct)
-	def keyValueWriter(name: String, keyValueModel: KeyValueModel) = apply(name, keyValueModel, GenericKeyValueProduct)
-	def hbaseWriter(name: String, keyValueModel: KeyValueModel) = apply(name, keyValueModel, HBaseProduct)
-	def topicWriter(name: String, topicModel: TopicModel) = apply(name, topicModel, GenericTopicProduct)
-	def kafkaWriter(name: String, topicModel: TopicModel) = apply(name, topicModel, KafkaProduct)
-	def rawWriter(name: String, rawModel: RawModel) = apply(name, rawModel, RawProduct)
-	def websocketWriter(name: String, websocketModel: WebsocketModel) = apply(name, websocketModel, WebSocketProduct)
-	def consoleWriter(name: String) = apply(name, None, ConsoleProduct)
+	def indexWriter(name: String, indexModel: IndexModel, options: Map[String, String] = Map.empty) =
+    apply(name, indexModel, GenericIndexProduct, options)
+	def elasticWriter(name: String, indexModel: IndexModel, options: Map[String, String] = Map.empty) =
+    apply(name, indexModel, ElasticProduct, options)
+	def solrWriter(name: String, indexModel: IndexModel, options: Map[String, String] = Map.empty) =
+    apply(name, indexModel, SolrProduct, options)
+	def keyValueWriter(name: String, keyValueModel: KeyValueModel, options: Map[String, String] = Map.empty) =
+    apply(name, keyValueModel, GenericKeyValueProduct, options)
+	def hbaseWriter(name: String, keyValueModel: KeyValueModel, options: Map[String, String] = Map.empty) =
+    apply(name, keyValueModel, HBaseProduct, options)
+	def topicWriter(name: String, topicModel: TopicModel, options: Map[String, String] = Map.empty) =
+    apply(name, topicModel, GenericTopicProduct, options)
+	def kafkaWriter(name: String, topicModel: TopicModel, options: Map[String, String] = Map.empty) =
+    apply(name, topicModel, KafkaProduct, options)
+	def rawWriter(name: String, rawModel: RawModel, options: Map[String, String] = Map.empty) =
+    apply(name, rawModel, RawProduct, options)
+	def websocketWriter(name: String, websocketModel: WebsocketModel, options: Map[String, String] = Map.empty) =
+    apply(name, websocketModel, WebSocketProduct, options)
+	def consoleWriter(name: String, options: Map[String, String] = Map.empty) =
+    apply(name, None, ConsoleProduct, options)
 }
