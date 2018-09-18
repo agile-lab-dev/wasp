c9d6f4c707131495ce23d7825766cd33fd149204
Nicolò Bidotti <nicolo.bidotti@gmail.com>
2018-09-18 17:31:34 +0200

    Fix setup.py

--- tools/release-note-generator/setup.py
+++ tools/release-note-generator/setup.py
@@ -1,11 +1,19 @@
 import pip
+try: # for pip >= 10
+   from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+   from pip.req import parse_requirements
+try: # for pip >= 10
+  from pip._internal import download
+except ImportError: # for pip <= 9.0.3
+  from pip import download
 from setuptools import setup, find_packages
 
 
 links = []  # for repo urls (dependency_links)
 requires = []  # for package names
 
-requirements = pip.req.parse_requirements('requirements.txt', session=pip.download.PipSession())
+requirements = parse_requirements('requirements.txt', session=download.PipSession())
 
 for item in requirements:
     if getattr(item, 'url', None):  # older pip has url
