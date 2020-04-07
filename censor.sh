fa25af6df64495469abe16133191e147ca49fe0a
Antonio Murgia <antonio.murgia@agilelab.it>
2020-04-07 11:25:16 +0000

    [#292] Censor CI step is broken
    # New features and improvements
    
    censor step in MR CI builds now works correctly!
    
    # Breaking changes
    
    None.
    
    # Migration
    
    None.
    
    # Bug fixes
    
    Fix MR setup bug that prevented any MR CI Build to pass because the env variable CI_MERGE_REQUEST_TARGET_BRANCH_SHA was always empty.
    
    # Related issue
    
    Closes #292

--- censor.sh
+++ censor.sh
@@ -1,14 +1,14 @@
-if [ -z "${CI_MERGE_REQUEST_TARGET_BRANCH_SHA}" ]
+if [ -z "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}" ]
 then
   echo "Not something that can be merged with this MR!"
   exit -1
 fi
-echo "base-commit: ${CI_MERGE_REQUEST_TARGET_BRANCH_SHA}"
+echo "base-commit: ${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}"
 echo "target-commit ${CI_COMMIT_SHA}"
 
 /opt/docker/bin/censor ${CENSOR_ARGS} \
 --repo-path '.' \
---base-commit ${CI_MERGE_REQUEST_TARGET_BRANCH_SHA} \
+--base-commit origin/${CI_MERGE_REQUEST_TARGET_BRANCH_NAME} \
 --target-commit ${CI_COMMIT_SHA} \
 --exclude 'documentation/icons/WASP_logo.pdf' \
 --exclude 'documentation/diagrams/statemachines.png' \
