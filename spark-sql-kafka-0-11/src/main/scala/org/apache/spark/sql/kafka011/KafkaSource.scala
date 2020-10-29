68015df428bc622718b2f0333c6636fe5aff7779
Andrea Fonti <andrea.fonti@agilelab.it>
2020-10-29 11:54:39 +0000

    [#419] fetchLatestOffsets return a wrong offset
    # New features and improvements
    
    None
    
    # Breaking changes
    
    None
    
    # Migration
    
    None
    
    # Bug fixes
    
    Backported SPARK-26267
    https://issues.apache.org/jira/browse/SPARK-26267
    https://github.com/apache/spark/pull/23324
    
    # Related issue
    
    Closes #419

--- spark-sql-kafka-0-11/src/main/scala/org/apache/spark/sql/kafka011/KafkaSource.scala
+++ spark-sql-kafka-0-11/src/main/scala/org/apache/spark/sql/kafka011/KafkaSource.scala
@@ -130,7 +130,7 @@ private[kafka011] class KafkaSource(
     metadataLog.get(0).getOrElse {
       val offsets = startingOffsets match {
         case EarliestOffsetRangeLimit => KafkaSourceOffset(kafkaReader.fetchEarliestOffsets())
-        case LatestOffsetRangeLimit => KafkaSourceOffset(kafkaReader.fetchLatestOffsets())
+        case LatestOffsetRangeLimit => KafkaSourceOffset(kafkaReader.fetchLatestOffsets(None))
         case SpecificOffsetRangeLimit(p) => fetchAndVerify(p)
       }
       metadataLog.add(0, offsets)
@@ -163,7 +163,9 @@ private[kafka011] class KafkaSource(
     // Make sure initialPartitionOffsets is initialized
     initialPartitionOffsets
 
-    val latest = kafkaReader.fetchLatestOffsets()
+    val latest = kafkaReader.fetchLatestOffsets(
+      currentPartitionOffsets.orElse(Some(initialPartitionOffsets))
+    )
     val offsets = maxOffsetsPerTrigger match {
       case None =>
         latest
