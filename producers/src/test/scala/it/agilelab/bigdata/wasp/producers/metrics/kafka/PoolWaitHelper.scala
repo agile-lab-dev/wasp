d4aacb291c4bc114c5dcb77a6d3ca0511997d70f
Antonio Murgia <antonio.murgia@agilelab.it>
2026-06-23 16:50:26 +0200

    Increase default deadline for awaitPoolEntry to 30 seconds

--- producers/src/test/scala/it/agilelab/bigdata/wasp/producers/metrics/kafka/PoolWaitHelper.scala
+++ producers/src/test/scala/it/agilelab/bigdata/wasp/producers/metrics/kafka/PoolWaitHelper.scala
@@ -6,7 +6,7 @@ import scala.annotation.tailrec
 trait PoolWaitHelper extends Assertions {
 
   @tailrec
-  final def awaitPoolEntry(key: String, deadlineMs: Long = System.currentTimeMillis() + 10000): Unit = {
+  final def awaitPoolEntry(key: String, deadlineMs: Long = System.currentTimeMillis() + 30000): Unit = {
     if (!Constants.offsetCheckerPool.contains(key)) {
       if (System.currentTimeMillis() > deadlineMs)
         fail(s"Timed out waiting for offsetCheckerPool entry '$key'")
